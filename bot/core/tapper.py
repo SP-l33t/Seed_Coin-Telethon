ca3bba1ef82ecef7b554613d3ca6d1e85e6a314a
Slayer <slayerpro27@gmail.com>
2024-09-28 21:25:11 +0300

    Re-added broken session handling

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -70,6 +70,11 @@ class Tapper:
 
         self._webview_data = None
 
+        if self.proxy:
+            proxy = Proxy.from_str(self.proxy)
+            proxy_dict = proxy_utils.to_telethon_proxy(proxy)
+            self.tg_client.set_proxy(proxy_dict)
+
     def log_message(self, message) -> str:
         return f"<light-yellow>{self.session_name}</light-yellow> | {message}"
 
@@ -82,37 +87,36 @@ class Tapper:
 
         return user_agent
 
+    async def initialize_webview_data(self):
+        if not self._webview_data:
+            while True:
+                try:
+                    peer = await self.tg_client.get_input_entity('seed_coin_bot')
+                    input_bot_app = InputBotAppShortName(bot_id=peer, short_name="app")
+                    self._webview_data = {'peer': peer, 'app': input_bot_app}
+                    break
+                except FloodWaitError as fl:
+                    fls = fl.seconds
+
+                    logger.warning(self.log_message(f"FloodWait {fl}. Waiting {fls}s"))
+                    await asyncio.sleep(fls + 3)
+
+                except (UnauthorizedError, AuthKeyUnregisteredError):
+                    raise InvalidSession(f"{self.session_name}: User is unauthorized")
+                except (UserDeactivatedError, UserDeactivatedBanError, PhoneNumberBannedError):
+                    raise InvalidSession(f"{self.session_name}: User is banned")
+
     async def get_tg_web_data(self) -> str:
-
-        if self.proxy:
-            proxy = Proxy.from_str(self.proxy)
-            proxy_dict = proxy_utils.to_telethon_proxy(proxy)
-        else:
-            proxy_dict = None
-        self.tg_client.set_proxy(proxy_dict)
-
-        self.tg_client.proxy = proxy_dict
-
         tg_web_data = None
         with self.lock:
-            async with self.tg_client as client:
-                if not self._webview_data:
-                    while True:
-                        try:
-                            peer = await client.get_input_entity('seed_coin_bot')
-                            input_bot_app = InputBotAppShortName(bot_id=peer, short_name="app")
-                            self._webview_data = {'peer': peer, 'app': input_bot_app}
-                            break
-                        except FloodWaitError as fl:
-                            fls = fl.seconds
-
-                            logger.warning(self.log_message(f"FloodWait {fl}"))
-                            logger.info(self.log_message(f"Sleep {fls}s"))
-                            await asyncio.sleep(fls + 3)
+            try:
+                if not self.tg_client.is_connected():
+                    await self.tg_client.connect()
+                await self.initialize_webview_data()
 
                 ref_id = settings.REF_ID if random.randint(0, 100) <= 85 else "525256526"
 
-                web_view = await client(messages.RequestAppWebViewRequest(
+                web_view = await self.tg_client(messages.RequestAppWebViewRequest(
                     **self._webview_data,
                     platform='android',
                     write_allowed=True,
@@ -121,6 +125,17 @@ class Tapper:
 
                 auth_url = web_view.url
                 tg_web_data = unquote(string=auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
+
+            except InvalidSession:
+                raise
+
+            except Exception as error:
+                log_error(self.log_message(f"Unknown error during Authorization: {type(error).__name__}"))
+                await asyncio.sleep(delay=3)
+
+            finally:
+                if self.tg_client.is_connected():
+                    await self.tg_client.disconnect()
 
         return tg_web_data
 
